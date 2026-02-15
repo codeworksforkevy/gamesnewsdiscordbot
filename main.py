ef8152b12056bfd9978e0b54ecc76e78c20e585d
codeworksforkevy <simreyalcin@yahoo.com>
2026-02-15 22:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import asyncio
 import aiohttp
 import discord
 from discord.ext import commands
-from config import DISCORD_TOKEN, GUILD_ID
+from config import DISCORD_TOKEN
 
 from commands.free_games import register_free_games
 from commands.membership import register_luna
 from commands.discounts import register_discounts
 from commands.twitch_badges import register_twitch_badges
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -23,13 +24,15 @@ async def on_ready():
     print(f"Bot ready: {bot.user}")
 
     if not commands_loaded:
+        # Register all commands AFTER bot is ready
         await register_free_games(bot, session)
         await register_luna(bot, session)
         await register_discounts(bot, session)
         await register_twitch_badges(bot, session)
 
-        await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
-        print("Slash commands synced.")
+        # Global sync (no guild ID)
+        await bot.tree.sync()
+        print("Global slash commands synced.")
 
         commands_loaded = True
 
@@ -47,3 +50,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
