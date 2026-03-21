7133208e42f509cd64540ac013301dd380b7ecc7
codeworksforkevy <simreyalcin@yahoo.com>
2026-03-21 05:23:12 +0300

    Update redis_client.py

--- services/redis_client.py
+++ services/redis_client.py
@@ -1,6 +1,9 @@
-import redis.asyncio as redis
 import os
+import redis.asyncio as redis
 
 REDIS_URL = os.getenv("REDIS_URL")
+
+if not REDIS_URL:
+    raise ValueError("REDIS_URL is not set in environment variables")
 
 redis = redis.from_url(REDIS_URL, decode_responses=True)
